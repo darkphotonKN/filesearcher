ea437b5ac0b7c9f380094aa3d146ba7ac2b25f7e
Kranti <darkphoton20@gmail.com>
2024-07-19 04:25:44 +0800

    Added concurrency to more effeciently find files.

--- internal/filesearcher.go
+++ internal/filesearcher.go
@@ -8,7 +8,18 @@ import (
 	"sync"
 )
 
+type searchfile struct {
+	found   bool
+	name    string
+	dirName string
+}
+
+// channel that indicates the file was found
+var fileFoundChan chan searchfile = make(chan searchfile)
+
 func Init() {
+	var wg sync.WaitGroup
+
 	fmt.Println()
 	fmt.Println("File Searcher")
 	fmt.Println("-------------")
@@ -18,61 +29,92 @@ func Init() {
 		return
 	}
 
-	dir := os.Args[0]
+	dir := os.Args[1]
 
-	searchTerm := os.Args[1]
+	searchTerm := os.Args[2]
 	fmt.Println("Target Directory:", dir)
 	fmt.Println("Search Term:", searchTerm)
 
-	found, err := searchFiles(dir, searchTerm)
+	wg.Add(1)
+	go func() {
+		defer wg.Done() // remove counter when this concurrenly running thread of concurrently
+		// running threads are done
 
-	if err != nil {
-		log.Fatal("Error when searching files:", err)
+		// keep this concurrently running until its done
+		err := searchFiles(dir, searchTerm, &wg)
+
+		if err != nil {
+			log.Fatal("Error when searching files:", err)
+		}
+	}()
+
+	// concurrently wait for all goroutines to finish
+	go func() {
+		wg.Wait()
+		close(fileFoundChan)
+	}()
+
+	result_file := <-fileFoundChan
+
+	if result_file.found {
+		fmt.Println("File was found.")
+		fmt.Printf("File name: %s, directory name: %s", result_file.name, result_file.dirName)
+	} else {
+		fmt.Println("File was not found.")
 	}
-
-	fmt.Println("File was found:", found)
 }
 
 // return if file is found
-func searchFiles(dir string, searchTerm string) (bool, error) {
+func searchFiles(dir string, searchTerm string, wg *sync.WaitGroup) error {
+	defer wg.Done()
 
 	file, err := os.Open(dir)
+	defer file.Close()
 
 	if err != nil {
 		fmt.Println("error while reading file:", err)
-		return false, err
+		return err
 	}
-
-	defer file.Close()
 
 	entries, err := file.ReadDir(-1)
 
-	if err != nil {
-		fmt.Println("error while reading all directories")
-		return false, err
-	}
-
 	for _, entry := range entries {
-		var wg *sync.WaitGroup
+
 		// skip if target is not a file but a directory
 		if entry.IsDir() {
+			wg.Add(1) // increment wait group before new goroutine
+
 			// if its a directory we start a goroutine to search for the file there
 			nestedPath := filepath.Join(dir, entry.Name())
 
-			wg.Add(1)
-			go searchFiles(nestedPath, searchTerm)
-			wg.Wait()
+			go searchFiles(nestedPath, searchTerm, wg)
+
+			// skip the rest of the loop as we aren't matching folders
 			continue
 		}
 
-		fmt.Print("entry.Name:", entry.Name())
-		fmt.Println(" compared to search term:", searchTerm)
+		// found matching name
+		fmt.Println()
+		fmt.Println("SEARCHING IN DIRECTORY:", dir)
+		fmt.Println("FILE NAME:", entry.Name())
+		fmt.Println("SEARCH TERM:", searchTerm)
+		fmt.Println()
 		if entry.Name() == searchTerm {
-			wg.Done()
-			return true, nil
-		}
 
+			fileFoundChan <- searchfile{
+				name:    entry.Name(),
+				dirName: dir,
+				found:   true,
+			}
+
+			// close channel after we found the target file
+			close(fileFoundChan)
+		}
+	}
+	if err != nil {
+		fmt.Println("error while reading all directories")
+		return err
 	}
 
-	return false, nil
+	return nil
 }
